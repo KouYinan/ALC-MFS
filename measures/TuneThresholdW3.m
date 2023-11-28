80dfca5f35b33c24deaf8bb38c48c8195354c9c9
KouYinan <145577813+KouYinan@users.noreply.github.com>
2023-11-28 19:43:59 +0800

    Delete measures directory

--- measures/TuneThresholdW3.m
+++
@@ -1,4 +0,0 @@
-function threshold = TuneThresholdW3(train_data, train_target, model)
-    Outputs_tr = train_data * model.w;
-    [threshold,  ~] = TuneThreshold( Outputs_tr', train_target, 1, 1);
-end
